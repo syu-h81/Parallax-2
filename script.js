6c2c3d1d72907fd49abccff5b490e73a41be1f5e
syu-h81 <58079996+syu-h81@users.noreply.github.com>
2020-07-06 17:14:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ $(function() {
   $('.menu-icon').click(function() {
     $('.menu-nav-wrapper').slideToggle();
   });
+  
+   $(window).scroll(function(){
+    var y = $(this).scrollTop();
+    $('#ls-bg').css('background-position', '0 ' + parseInt( -y / 4 ) + 'px');
+    //$('#bg2-ls-img').css('background-position', '0 ' + parseInt( -y / 3 ) + 'px');
+    $('#safety-bg').css('background-position', '0 ' + parseInt( -y / 7 ) + 'px');
+    //$('#bg02').css('background-position', '0 ' + parseInt( -y / 50 ) + 'px');
+    //$('#bg03').css('background-position', '0 ' + parseInt( -y / 200 ) + 'px');
+  });
 });
 
 
