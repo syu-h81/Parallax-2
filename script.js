e8e27e8b41b8bc5cee065d81a4eac00ff6bf51af
syu-h81 <58079996+syu-h81@users.noreply.github.com>
2020-07-06 18:03:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(function() {
    $(window).scroll(function(){
     var y = $(this).scrollTop();
     $('#ls-bg').css('background-position', '0 ' + parseInt( -y / 4 ) + 'px');
-     $('#ls').css('background-position', '0 ' + parseInt( -y * 9 ) + 'px');
+    $('#ls').css('background-position', '0 ' + parseInt( -y / 8 ) + '%');
     //$('#bg2-ls-img').css('background-position', '0 ' + parseInt( -y / 3 ) + 'px');
     //$('#safety-bg').css('background-position', '0 ' + parseInt( -y / 7 ) + 'px');
     //$('#bg02').css('background-position', '0 ' + parseInt( -y / 50 ) + 'px');
